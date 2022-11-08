302f49a5b9a9883686eb558983e7b7582b0072c6
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-11-08 11:02:47 -0500

    working on fisrt carousel menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,39 +7,10 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='nav-left'>
         <Menu />
-        {/* <ul>
-          <li>
-            <a href='/#' alt='shop'>
-              Shop
-            </a>
-          </li>
-          <li>
-            <a href='/#' alt='about'>
-              About
-            </a>
-          </li>
-          <li>
-            <a href='/#' alt='lookbook'>
-              Lookbook
-            </a>
-          </li>
-        </ul> */}
       </div>
       <div className='nav-center title'>fedora</div>
       <div className='nav-right'>
-        <ul>
-          <UserMenu />
-          {/* <li>
-            <a href='/#' alt='account'>
-              My Account
-            </a>
-          </li>
-          <li>
-            <a href='/#' alt='cart'>
-              Cart (0)
-            </a>
-          </li> */}
-        </ul>
+        <UserMenu />
       </div>
     </div>
   );
